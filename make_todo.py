d68fb95c5f6c18a8323e7ae646d7b71ed90c6ca1
gamasenninn <gamasenninn@yahoo.co.jp>
2023-12-22 16:13:21 +0900

    TODO機能追加

---
+++ make_todo.py
@@ -0,0 +1,59 @@
+import sys
+import glob
+import os
+import json
+
+def load_replacement_dict(file_path):
+    replacement_dict = {}
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            parts = line.strip().split(':')
+            if len(parts) == 2:
+                replacement_dict[parts[0]] = parts[1]
+    return replacement_dict
+
+def replace_words(text, replacement_dict):
+    for word, replacement in replacement_dict.items():
+        text = text.replace(word, replacement)
+    return text
+
+def make_todo(dir_path):
+    files = glob.glob(os.path.join(dir_path, "*segment_*todo.txt"))
+    print(files)
+    output_file =  os.path.join(dir_path, "todo.txt")
+
+        # 読み替え辞書をファイルから読み込む
+    replacement_dict = load_replacement_dict("replacement_dict.txt")
+
+    # ファイルを最初に上書きモードで開き、空にする
+    open(output_file, 'w', encoding='utf-8').close()
+
+    # 各ファイルを読み込み、出力ファイルに追記
+    for file in files:
+        with open(file, 'r', encoding='utf-8') as f:
+            print(file)
+            data = json.load(f)
+            #combined_minutes = ["□ {}/{}\n".format(item['title'], item['Priority']) for item in data['todo']]
+            combined_minutes = ["□ {}\n".format(replace_words(item['title'], replacement_dict)) for item in data['todo']]
+            
+            # この時点でファイルを追記モードで開く
+            with open(output_file, 'a', encoding='utf-8') as out_file:
+                for minute in combined_minutes:
+                    print(minute)
+                    out_file.write(minute + "\n")
+                      
+
+
+
+if __name__ == "__main__":
+    # コマンドライン引数のチェック
+    if len(sys.argv) != 2:
+        print("Usage: python make_minutes.py <video_dirpath>")
+        sys.exit(1)
+
+    # コマンドライン引数からビデオファイルのあるディレクトリを取得
+    dir_path = sys.argv[1]
+
+    # 
+
+    make_todo(dir_path)

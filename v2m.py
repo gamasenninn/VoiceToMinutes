2d20e916f6cb121f081c4cd38b792d9adb0746dd
gamasenninn <gamasenninn@yahoo.co.jp>
2024-04-14 18:25:36 +0900

    微修正

--- v2m.py
+++ v2m.py
@@ -25,10 +25,19 @@ def main():
     print(f"path_filname_srt: {path_filename_srt}")
 
     # 各種スクリプトを実行
+    print("----- MP4からMP3に変換 ---------")
     run_script('mp42mp3.py', fullpath)
+
+    print("----- 文字起こしをしてSRT形式のテキストに変換 ---------")
     run_script('tran_gladia.py', path_filename_mp3)
+
+    print("----- セグメントのシナリオを作成する ---------")
     run_script('srt2seg.py', path_filename_srt)
+
+    print("----- セグメントから議事録を起こす---------")
     run_script('seg2txt.py', path_filename_srt)
+
+    print("----- 議事録とyouubeキャプチャーを作成する ---------")
     run_script('make_minutes.py', path)
 
 def run_script(script_name, argument):
